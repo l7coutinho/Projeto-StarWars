97d0e55059426a7854501d34abe6e76ea344eb75
Lucas Coutinho <lucxscout@gmail.com>
2023-10-18 15:40:40 -0300

    fix lint

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -3,7 +3,6 @@ import PlanetTable from '../PlanetTable';
 import FilterContext from '../../context/ProviderContext';
 import { FilterType, ResultType } from '../../types';
 
-
 function Table() {
   const { data } = useContext(FilterContext);
   const [inputValue, setInputValue] = useState('');
@@ -25,11 +24,14 @@ function Table() {
   };
 
   const handleClickButton = () => {
-    const newFilter = { column: selectedColumn, comparison: selectedComparison, value: selectedValue };
+    const newFilter = {
+      column: selectedColumn, comparison: selectedComparison, value: selectedValue,
+    };
     const newFilters = [...filters, newFilter];
     setFilters(newFilters);
 
-    const updatedAvailableColumns = availableColumns.filter(column => column !== selectedColumn);
+    const updatedAvailableColumns = availableColumns
+      .filter((column) => column !== selectedColumn);
     setAvailableColumns(updatedAvailableColumns);
 
     const filteredPlanets2 = filteredPlanetArray.filter((planet: any) => {
@@ -79,14 +81,15 @@ function Table() {
   };
 
   const removeAllFilters = () => {
-    setAvailableColumns(['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water']);
+    setAvailableColumns(
+      ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'],
+    );
     setFilters([]);
     setFilteredPlanetArray(data);
   };
 
-  const filteredPlanets = filteredPlanetArray.filter((planet: any) =>
-    planet.name.toLowerCase().includes(inputValue)
-  );
+  const filteredPlanets = filteredPlanetArray
+    .filter((planet: any) => planet.name.toLowerCase().includes(inputValue));
 
   return (
     <div>
@@ -94,27 +97,27 @@ function Table() {
         <input
           type="text"
           data-testid="name-filter"
-          value={inputValue}
-          onChange={handleChangeInput}
+          value={ inputValue }
+          onChange={ handleChangeInput }
         />
       </div>
 
       <div>
         <select
           data-testid="column-filter"
-          value={selectedColumn}
-          onChange={(e) => setSelectedColumn(e.target.value)}
+          value={ selectedColumn }
+          onChange={ (e) => setSelectedColumn(e.target.value) }
         >
           {availableColumns.map((column) => (
-            <option key={column} value={column}>
+            <option key={ column } value={ column }>
               {column}
             </option>
           ))}
         </select>
         <select
           data-testid="comparison-filter"
-          value={selectedComparison}
-          onChange={(e) => setSelectedComparison(e.target.value)}
+          value={ selectedComparison }
+          onChange={ (e) => setSelectedComparison(e.target.value) }
         >
           <option value="maior que">maior que</option>
           <option value="menor que">menor que</option>
@@ -123,26 +126,25 @@ function Table() {
         <input
           type="text"
           data-testid="value-filter"
-          value={selectedValue}
-          onChange={(e) => setSelectedValue(e.target.value)}
+          value={ selectedValue }
+          onChange={ (e) => setSelectedValue(e.target.value) }
         />
-        <button data-testid="button-filter" onClick={handleClickButton}>
+        <button data-testid="button-filter" onClick={ handleClickButton }>
           Filter
         </button>
       </div>
 
-
-      <PlanetTable data={filteredPlanets} />
+      <PlanetTable data={ filteredPlanets } />
 
       <div>
         {filters.map((filter, index) => (
-          <div key={index} data-testid="filter">
+          <div key={ index } data-testid="filter">
             {`${filter.column} ${filter.comparison} ${filter.value}`}
-            <button onClick={() => removeFilter(index)}>X</button>
+            <button onClick={ () => removeFilter(index) }>X</button>
           </div>
         ))}
 
-        <button data-testid="button-remove-filters" onClick={removeAllFilters}>
+        <button data-testid="button-remove-filters" onClick={ removeAllFilters }>
           Remover todas filtragens
         </button>
       </div>
--- src/types.ts
+++ src/types.ts
@@ -18,4 +18,4 @@ export type FilterType = {
   column: string,
   comparison: string,
   value: string,
-}
+};
